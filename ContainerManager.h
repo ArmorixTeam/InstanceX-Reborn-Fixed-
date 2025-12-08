3c4f47fb92ee2caba45fe286d63195cf82364a2a
Zodac <devblueb@gmail.com>
2025-12-08 14:02:42 -0500

    Update ContainerManager.h

--- ContainerManager.h
+++ ContainerManager.h
@@ -3,4 +3,5 @@
 @interface ContainerManager : NSObject
 + (instancetype)shared;
 - (BOOL)prepareLaunchEnvironmentForContainer:(NSString *)containerID intoEnv:(NSMutableDictionary *)env;
+- (NSString *)createContainerForBundle:(NSString *)bundleID instanceIndex:(NSInteger)index;
 @end
